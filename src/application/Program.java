6de6384c32b8a59ae631b8aecedf78dfe0cf027a
FabioL-96 <fabiolucio21@outlook.com.br>
2022-08-25 20:46:57 -0300

    Projeto atualizado <uptaded project/>

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,45 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Bill;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		Bill orderNote = new Bill();
+
+		do {
+			System.out.print("Sexo: ");
+			orderNote.gender = sc.next().charAt(0);
+		} while (orderNote.gender != 'M' && orderNote.gender != 'F');
+
+		System.out.print("Quantidade de cervejas: ");
+		orderNote.beer = sc.nextInt();
+
+		System.out.print("Quantidade de refrigerantes: ");
+		orderNote.softDrink = sc.nextInt();
+
+		System.out.print("Quantidade de espetinhos: ");
+		orderNote.barbecue = sc.nextInt();
+
+		System.out.println();
+		System.out.println("RELATÓRIO: ");
+
+		System.out.printf("Consumo = R$ %.2f\n", orderNote.feeding());
+		if (orderNote.cover() == 4.0) {
+			System.out.println("Couvert = R$ 4.00");
+		} else {
+			System.out.println("Isento de Couvert");
+		}
+		System.out.printf("Ingresso = R$ %.2f\n", orderNote.ticket());
+
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f\n", orderNote.total());
+		sc.close();
 	}
 
 }
